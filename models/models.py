1c4d8b6aefcdfb52831393ba3ff5bd482fe1700a
RuBisCO28 <54470624+RuBisCO28@users.noreply.github.com>
2020-06-22 00:49:09 +0900

    Add CashFlow graph

--- models/models.py
+++ models/models.py
@@ -5,20 +5,22 @@ from datetime import datetime
 class EdinetCodeInfo(Base):
     __tablename__ = 'edinetcodeinfo'
     edinetcode = Column(String(128), primary_key=True)
-    capital = Column(Integer)
     fiscalperiod = Column(String(128))
     submitter = Column(String(128))
     businesstype = Column(String(128))
     securitiescode = Column(Integer)
+    url = Column(String(128))
+    description = Column(String(128))
 
-    def __init__(self, edinetcode=None, capital=None, fiscalperiod=None, submitter=None, 
-    businesstype=None, securitiescode=None):
+    def __init__(self, edinetcode=None, fiscalperiod=None, submitter=None, 
+    businesstype=None, securitiescode=None, url=None, description=None):
         self.edinetcode = edinetcode
-        self.capital = capital
         self.fiscalperiod = fiscalperiod
         self.submitter = submitter
         self.businesstype = businesstype
         self.securitiescode = securitiescode
+        self.url = url
+        self.description = description
 
     def __repr__(self):
         return '<Title %r>' % (self.edinetcode)
@@ -36,12 +38,23 @@ class FinanceInfo(Base):
     operatingincome = Column(Integer)
     nonoperatingincome = Column(Integer)
     nonoperatingexpenses = Column(Integer)
-    ordinaryIncome = Column(Integer)
+    ordinaryincome = Column(Integer)
     extraordinaryincome = Column(Integer)
     extraordinaryloss = Column(Integer)
     incomebeforeincometaxes = Column(Integer)
     incometaxes = Column(Integer)
     profitloss = Column(Integer)
+    currentassets = Column(Integer)
+    noncurrentassets = Column(Integer)
+    propertyplantandequipment = Column(Integer)
+    intangibleassets = Column(Integer)
+    investmentsAndotherAssets = Column(Integer)
+    currentliabilities = Column(Integer)
+    noncurrentliabilities = Column(Integer)
+    netAssets = Column(Integer)
+    scf = Column(Integer)
+    icf = Column(Integer)
+    fcf = Column(Integer)
 
     def __init__(self, rid=None, edinetcode=None, securitiescode=None, enddate=None, netsales=None):
         self.rid = rid
@@ -55,12 +68,23 @@ class FinanceInfo(Base):
         self.operatingincome = operatingincome
         self.nonoperatingincome = nonoperatingincome
         self.nonoperatingexpenses = nonoperatingexpenses
-        self.ordinaryIncome = ordinaryIncome
+        self.ordinaryincome = ordinaryincome
         self.extraordinaryincome = extraordinaryincome
         self.extraordinaryloss = extraordinaryloss
         self.incomebeforeincometaxes = incomebeforeincometaxes
         self.incometaxes = incometaxes
         self.profitloss = profitloss
+        self.currentassets = currentassets
+        self.noncurrentassets = noncurrentassets
+        self.propertyplantandequipment = propertyplantandequipment
+        self.intangibleassets = intangibleassets
+        self.investmentsAndotherAssets = investmentsAndotherAssets
+        self.currentliabilities = currentliabilities
+        self.noncurrentliabilities = noncurrentliabilities
+        self.netAssets = netAssets
+        self.scf = scf
+        self.icf = icf
+        self.fcf = fcf
         
     def __repr__(self):
         return '<Title %r>' % (self.edinetcode)
